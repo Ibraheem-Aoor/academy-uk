edca3b58b523194676be73af826e9f4087205c30
Ibraheem <ibraheem.alaoor@hotmail.com>
2024-09-26 15:02:15 +0300

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1047,41 +1047,4 @@
     </div>
     <!-- Blog End -->
 
-    <!-- Testimonial Start -->
-    <div class="container-fluid testimonial py-5">
-        <div class="container py-5">
-            <div class="mx-auto text-center mb-5" style="max-width: 900px;">
-                <h5 class="section-title px-3">Testimonial</h5>
-                <h1 class="mb-0">Our Clients Say!!!</h1>
-            </div>
-            <div class="testimonial-carousel owl-carousel">
-                @foreach ($testmonials as $testmonial)
-                    <div class="testimonial-item text-center rounded pb-4">
-                        <div class="testimonial-comment bg-light rounded p-4">
-                            <p class="text-center mb-5">
-                                {{ $testmonial->feedback }}
-                            </p>
-                        </div>
-                        <div class="testimonial-img p-1">
-                            <img src="{{ getImageUrl($testmonial->image) }}" class="img-fluid rounded-circle"
-                                alt="Image" width="50px" height="50px">
-                        </div>
-                        <div style="margin-top: -35px;">
-                            <h5 class="mb-0">{{ $testmonial->name }}</h5>
-                            <p class="mb-0">{{ $testmonial->location }}</p>
-                            <div class="d-flex justify-content-center">
-                                <i class="fas fa-star text-primary"></i>
-                                <i class="fas fa-star text-primary"></i>
-                                <i class="fas fa-star text-primary"></i>
-                                <i class="fas fa-star text-primary"></i>
-                                <i class="fas fa-star text-primary"></i>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-
-            </div>
-        </div>
-    </div>
-    <!-- Testimonial End -->
 @endsection
